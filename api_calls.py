44f54621d023dd1437fbbb5547a0f8c779a25437
Jonathan <3190208@mail.zih.tu-dresden.de>
2024-03-16 16:46:38 +0100

    Implement create empty, delete deposit

--- api_calls.py
+++ api_calls.py
@@ -1,4 +1,6 @@
+import os
 import json
+import curlify
 import requests
 from os import path
 from dataclasses import dataclass
@@ -18,41 +20,134 @@ class API:
             "files"
         ]
 
-    def _add_credentials(self, url):
-        return f"{url}?access_token={self.token}"
+    def default_info(self):
+        info = "{metadata:{ title: My generated Depot, upload_type: presentation, description: This is my first upload, creators: [ {name: Python, Script, affiliation: Zeno} ], }}"
+        return info
+
+    def json_content_header(self):
+        return {"Content-Type": "application/json"}
+
+    def multipart_content_header(self):
+        return {"Content-Type": "multipart/form-data"}
 
     def build_url(self, *args):
         url = path.join(self.endpoint, *args)
-        return self._add_credentials(url)
+        return url
+
+    def build_deposit_url(self, *args):
+        deposit_url = self.build_url("deposit", "depositions", *args)
+        return deposit_url
+    
+    def build_file_url(self, deposit_id, *args):
+        deposit_url = self.build_deposit_url(deposit_id, "files", *args)
+        return deposit_url
+
+    def authenticate(self, url):
+        return f"{url}?access_token={self.token}"
 
     def list_all_deposits(self):
-        url = self.build_url("deposit", "depositions")
+        route = self.build_url("deposit", "depositions")
+        url = self.authenticate(route)
         req = requests.get(url)
         data = req.json()
 
         for record in data:
-            for field in self.fields:
-                print(field)
-                print(json.dumps(record[field], indent=2))
-
-    def default_info(self):
-        info = """
-        {
-            "metadata": {
-                "title": "My generated Upload",
-                "upload_type": "presentation",
-                "upload_type": "presentation",
-                "creators": [
-                    {"name": "Python, Script", "affiliation": "Zeno"}
-                ],
-            }
-        }
-        """
-        return info
-
-    def set_header(self):
-        ...
+            print(record)
+            # for field in self.fields:
+            #     print(field)
+            #     print(json.dumps(record[field], indent=2))
 
     def create_deposit(self, metadata=None):
-        url = self.build_url("deposit", "depositions")
+        if not metadata:
+            metadata = json.dumps(self.default_info())
+            metadata = "{}"
+        header = self.json_content_header()
+        route = self.build_deposit_url()
+        url = self.authenticate(route)
+        req = requests.post(url, data=metadata, headers=header)
+        curl = curlify.to_curl(req.request)
+        print(curl)
+
+        return req.json()
+
+    def get_deposit(self, id):
+        identifier = str(id)
+        route = self.build_deposit_url(identifier)
+        url = self.authenticate(route)
+        req = requests.get(url)
+        return req.json()
+
+    def update_deposit(self, id, metadata=None):
+        if not metadata:
+            print("insufficient data for update")
+            print("using default")
+            # exit()
+        identifier = str(id)
+        route = self.build_deposit_url(identifier)
+        url = self.authenticate(route)
+        metadata = self.default_info()
         print(url)
+        print(metadata)
+        req = requests.put(url, data=json.dumps(metadata))
+        return req.json()
+
+    def delete_deposit(self, id):
+        identifier = str(id)
+        route = self.build_deposit_url(identifier)
+        url = self.authenticate(route)
+        req = requests.delete(url)
+        print(req.headers)
+
+    def get_files_of_deposit(self, id):
+        identifier = str(id)
+        route = self.build_deposit_url(identifier, "files")
+        url = self.authenticate(route)
+        req = requests.get(url)
+        return req.json()
+
+    def upload_file(self, deposit_id, file_path):
+        if not path.exists(file_path):
+            raise FileNotFoundError
+        if not path.isfile(file_path):
+            raise FileNotFoundError
+
+        deposit_id = str(deposit_id)
+        header = self.multipart_content_header()
+        route = self.build_file_url(deposit_id)
+        print(route)
+
+        url = self.authenticate(route)
+
+        _, file_name = path.split(file_path)
+        print(file_name)
+        fp = path.join(os.getcwd(), file_name)
+        print(fp)
+
+        files = {
+            'name': (None, file_name),
+            'file': open(fp, "rb")
+        }
+
+        params = {"-X": "POST"}
+
+        print(url)
+        req = requests.post(url, headers=header, files=files, params=params)
+        print(requests.post(url, headers=header, files=files, params=params))
+        response = req.json()
+        print(json.dumps(response, indent=4))
+
+
+# curl -i -H "Content-Type: application/json"
+# -X PUT 
+# --data '{"metadata": 
+#             {"title": "My first upload", 
+#             "upload_type": "poster", 
+#             "description": "This is my first upload",
+#             "arbitrary": "some more additional metadata",
+#             "creators": [
+#                 {"name": "Doe, John",
+#                 "affiliation": "Zenodo"}
+#             ]
+#         }
+#     }' 
+# https://zenodo.org/api/deposit/depositions/10817080\?access_token\=iVkHh6aGfWIOlgy1XcCx3SvBqtFS4XJhlL8yomqoZuzGGjO9ga0PsCIS53DI
--- main.py
+++ main.py
@@ -1,13 +1,84 @@
 import os
+import re
+import json
+import argparse
 from api_calls import API
 from dotenv import load_dotenv
 
 
-load_dotenv()
-api_key = os.getenv("ACCESS_TOKEN")
-wrapper = API(api_key)
+# records = wrapper.list_all_deposits()
+# print(records)
 
-records = wrapper.list_all_deposits()
+# deposit_id = 10817144
 
-# for record in records:
-print(records)
+# deposit = wrapper.get_files_of_deposit(deposit_id)
+# print(json.dumps(deposit, indent=4))
+
+# deposit = wrapper.get_deposit(deposit_id)
+# print(json.dumps(deposit, indent=4))
+
+# wrapper.upload_file(deposit_id, "./rli_dibt_windzone_vg_variant.json")
+
+# deposit = wrapper.get_deposit(deposit_id)
+# print(json.dumps(deposit, indent=4))
+
+def setup_parser():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-c', '--create_deposit', action="store_true",
+                        help="Create a new Deposit")
+    parser.add_argument('-d', '--deposit', help="Retrieve a deposit by id")
+    parser.add_argument('-f', '--files', help="List all files of a Deposit")
+    parser.add_argument('-u', '--upload', help="Upload a file to a Deposit")
+    parser.add_argument('-s', '--update', help="Update a Deposit")
+    parser.add_argument('-del', '--delete_deposit',
+                        help="Delete a deposit by id")
+    parser.add_argument('-ds', '--deposits', action="store_true",
+                        help="List all Deposits of a User")
+    return parser
+
+
+def parse_id(id):
+    non_number = re.compile(r'\D+')
+    match = re.findall(non_number, id)
+    if len(match) > 0:
+        print("Wrong ID: ", id)
+        exit()
+    return id
+
+
+def setup_api():
+    load_dotenv()
+    api_key = os.getenv("ACCESS_TOKEN")
+    wrapper = API(api_key)
+    return wrapper
+
+
+if __name__ == '__main__':
+    parser = setup_parser()
+    args = parser.parse_args()
+    api = setup_api()
+    if args.deposit:
+        depo_id = parse_id(args.deposit)
+        response = api.get_deposit(depo_id)
+        print(response)
+    if args.create_deposit:
+        response = api.create_deposit()
+        print(response)
+    if args.files:
+        depo_id = parse_id(args.files)
+        response = api.get_files_of_deposit(depo_id)
+        print(response)
+    if args.update:
+        depo_id = parse_id(args.update)
+        metadata = None
+        response = api.update_deposit(depo_id, metadata=metadata)
+        print(response)
+    if args.delete_deposit:
+        depo_id = parse_id(args.delete_deposit)
+        # response = api.get_deposit(depo_id)
+        resp = api.delete_deposit(depo_id)
+        print(resp)
+    if args.deposits:
+        api.list_all_deposits()
+    if args.upload:
+        api.list_all_deposits()
